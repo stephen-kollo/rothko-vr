baf6cbf051c4e1d376a40fdb01782bc1df1f0a18
Stephen <116318411+stephen-kollo@users.noreply.github.com>
2022-10-20 20:19:20 -0300

    Create uiController.js

---
+++ public/components/uiController.js
@@ -0,0 +1,17 @@
+function uiController(self) {
+    class uiClass{
+        constructor(){
+            this.ui = "hi";
+        }
+
+
+        initButton() {
+            
+        }
+    }
+
+    let newUI = new uiClass();
+    return newUI;
+}
+
+export default uiController;
